97f7c0340a92e2cecc828acea71ae7453c7e9edf
panpan31415 <panpan31415@gmail.com>
2021-02-16 00:14:18 +0100

    remove solution

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,5 @@
 def nine_nine_table():
-    a = 1
-    b = 1
-    while a <= 9:
-        b=1
-        while b <= a:
-            result = "{}*{}={} " 
-            print(result.format(a,b,a*b), end = "")
-            b+=1
-        a+=1
-        print("", end="\n")
-        
+  # write your code here
+
+    
 nine_nine_table()
